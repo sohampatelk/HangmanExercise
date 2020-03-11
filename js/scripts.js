91dfa0558476be0a8db3cd1534b31c62e628de5d
sohampatelk <sohampatelk@yahoo.com>
2020-03-10 20:05:45 -0600

    console hangman done

--- js/scripts.js
+++ js/scripts.js
@@ -21,25 +21,28 @@ for (var i = 0; i < word.length; i++) {
 console.log(answerArray);
 
 var remainingLetters = word.length;
-var wrongInput = 0;
+var wrongInput = 5;
 
-for(wrongInput = 0){
+while(wrongInput>0 && remainingLetters>0){
     //show player therir progress with answer array[] contacination
     alert(answerArray.join(" "));
 
     //get a guess from player
     var guess = prompt("Guess a letter or click cancel to stop playing");
-
+    
     for(var j=0;j<word.length;j++){
         if(word[j] === guess){
             answerArray[j] = guess;
             remainingLetters--;
+            wrongInput++;
         }  
-        
     }
+    wrongInput--;
 }
 
 if(remainingLetters == 0){
     alert(answerArray.join(" "));
     alert("Goodjob!! the answer is"+word);
-}else 
+}else if(wrongInput == 0){
+    alert("You fail");
+}
