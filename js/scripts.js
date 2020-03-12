a9038af6ccb094d7efff221f2f0dfdf324c42e29
sohampatelk <sohampatelk@yahoo.com>
2020-03-11 23:03:58 -0600

    added chances

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,23 @@ var word = words[Math.floor(Math.random() * words.length)];  */
 
 var word = prompt("What word you would like to give user for Hangman game:");
 var hint = prompt("What kind of hint you want to give user : ");
+var chances = prompt("How many chances you want to give user : ");
+
 
 // Set up the answer array for picked word for design "-------"" 
 var answerArray = [];
-/* for (var i = 0; i < word.length; i++) {
-    if(i==2 || i==5){
+for (var i = 0; i < word.length; i++) {
+    if(i==2){
         answerArray[i]=word[i];
     }else{
     answerArray[i] = "_";
     }
-} */
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
+} 
+
 
 console.log(answerArray);
 
 var remainingLetters = word.length;
-var chances = 5;
 
 //hint
 alert("The hint is : "+hint);
@@ -38,7 +37,7 @@ while(chances>0 && remainingLetters>0){
     alert(answerArray.join(" "));
 
     //get a guess from player
-    var guess = prompt("Guess a letter or click cancel to stop playing");
+    var guess = prompt("Guess a letter or click cancel to stop playing :");
     
     for(var j=0;j<word.length;j++){
         if(word[j] === guess){
