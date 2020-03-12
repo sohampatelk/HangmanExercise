f167451240dd3a6073be2ab9da9ce69873613af1
sohampatelk <sohampatelk@yahoo.com>
2020-03-11 22:31:57 -0600

    added propt for user and computer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var words = [
+/* var words = [
     "javascript",
     "monkey",
     "amazing",
@@ -6,26 +6,34 @@ var words = [
 ];
 
 // Pick a random word from upper array
-var word = words[Math.floor(Math.random() * words.length)]; 
+var word = words[Math.floor(Math.random() * words.length)];  */
 
-//var word = prompt("What word you would like to give user for Hangman game:");
+var word = prompt("What word you would like to give user for Hangman game:");
+var hint = prompt("What kind of hint you want to give user : ");
 
 // Set up the answer array for picked word for design "-------"" 
 var answerArray = [];
-for (var i = 0; i < word.length; i++) {
+/* for (var i = 0; i < word.length; i++) {
     if(i==2 || i==5){
         answerArray[i]=word[i];
     }else{
     answerArray[i] = "_";
     }
+} */
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
 }
 
 console.log(answerArray);
 
 var remainingLetters = word.length;
-var wrongInput = 5;
+var chances = 5;
 
-while(wrongInput>0 && remainingLetters>0){
+//hint
+alert("The hint is : "+hint);
+alert("No of chances to enter wrong letter : "+chances);
+
+while(chances>0 && remainingLetters>0){
     //show player therir progress with answer array[] contacination
     alert(answerArray.join(" "));
 
@@ -36,15 +44,16 @@ while(wrongInput>0 && remainingLetters>0){
         if(word[j] === guess){
             answerArray[j] = guess;
             remainingLetters--;
-            wrongInput++;
+            chances++;
         }  
     }
-    wrongInput--;
+    chances--;
 }
 
+//print for user if user is win or loose
 if(remainingLetters == 0){
     alert(answerArray.join(" "));
-    alert("Goodjob!! the answer is"+ word);
-}else if(wrongInput == 0){
+    alert("Goodjob!! the answer is : "+ word);
+}else if(chances == 0){
     alert("You fail");
 }
